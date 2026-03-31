3f59dfe8a641d30af1b42d8e3d687f8259c34bb7
Michael Davidsaver <mdavidsaver@gmail.com>
2026-03-31 16:50:50 -0700

    add autosave

--- iocBoot/iocEVT/st.cmd
+++ iocBoot/iocEVT/st.cmd
@@ -9,4 +9,18 @@ ospreyTimingIoc_registerRecordDeviceDriver(pdbbase)
 
 dbLoadRecords("../../db/ospreyEVT.db","P=$(P),NAME=EVT,IPADDR=$(EVT_IPADDR)")
 
+system "install -d as"
+
+set_savefile_path("$(PWD)", "/as")
+set_requestfile_path("$(PWD)", "/as")
+
+set_pass0_restoreFile("evt_settings.sav")
+set_pass1_restoreFile("evt_waveforms.sav")
+
 iocInit()
+
+makeAutosaveFileFromDbInfo("$(PWD)/as/evt_settings.req", "autosaveFields_pass0")
+makeAutosaveFileFromDbInfo("$(PWD)/as/evt_waveforms.req", "autosaveFields_pass1")
+
+create_monitor_set("evt_settings.req", 10 , "")
+create_monitor_set("evt_waveforms.req", 30 , "")
